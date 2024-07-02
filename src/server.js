9d6b44bd9f61b4256a64cca370c9971e44734ab7
gabrielabade <gabriela.bade@hotmail.com>
2024-07-02 16:55:04 -0300

    User Model Configuration

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-import app from './app'
+import app from './app';
 
-app.listen(3002, () => console.log("🚀 Server is running at port 3002.. "));
+app.listen(3002, () => console.log('🚀 Server is running at port 3002.. '));
