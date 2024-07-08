18c6e7613849ae45bf482ada8a23cd277f2ad5bc
gabrielabade <gabriela.bade@hotmail.com>
2024-07-08 14:49:20 -0300

    validating duplicate email

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,15 +19,25 @@ class UserController {
       admin: Yup.boolean(),
     });
 
-    try{
-    schema.validateSync(request.body, { abortEarly: false })
-    } catch(err) {
-      return response.status(400).json({error: err.errors})
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
     }
 
     const {
       name, email, password_hash, admin,
     } = request.body;
+
+    const userExists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
 
     const user = await User.create({
       id: v4(),
