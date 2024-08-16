f9c0eedcfa5bb20a4a77aeedfd2dd9c678de7b04
gabrielabade <gabriela.bade@hotmail.com>
2024-08-16 17:07:46 -0300

    Resolving connection error with CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { resolve } from 'node:path';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
@@ -7,6 +8,8 @@ import './database';
 class App {
   constructor() {
     this.app = express();
+
+    this.app.use(cors());
 
     this.middlewares();
 
